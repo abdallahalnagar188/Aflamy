41808dce4d237d6a60470ee7cfce37013d6a82d8
EUON <bdallhmsd807@gmail.com>
2024-10-08 16:16:15 +0300

    Add movies for Actors Add some updates

--- app/src/main/java/com/example/aflamy/presentation/ui/moviesDetails/MoviesForActorsFragment.kt
+++ app/src/main/java/com/example/aflamy/presentation/ui/moviesDetails/MoviesForActorsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.aflamy.presentation.ui.moviesDetails
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -85,6 +86,7 @@ class MoviesForActorsFragment : BaseFragment<FragmentSeeMorePopularMoviesBinding
 
 
 
+    @SuppressLint("SetTextI18n")
     private fun setupListeners() {
         binding.apply {
             topBar.ivBack.setOnClickListener {
